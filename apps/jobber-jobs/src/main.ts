70bb2406420c3a3fff100e683fe526a461d4def8
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-04-25 13:27:13 -0300

    feat(jobs): add jobber-jobs application with initial setup, configuration, and tests

--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -1,17 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.use(cookieParser());
+  const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
