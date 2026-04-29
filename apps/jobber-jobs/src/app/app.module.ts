d1d9ececb0e186d6a66075afed51dc690cd66b69
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-04-28 22:16:22 -0300

    feat(jobs): add JobsService and integrate DiscoveryModule in AppModule

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JobsModule } from '../jobs.module';
+import { JobsService } from './jobs/jobs.service';
+import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [ConfigModule, JobsModule, DiscoveryModule],
   controllers: [],
-  providers: [],
+  providers: [JobsService],
 })
 export class AppModule {}
---
+++ apps/jobber-jobs/src/app/jobs/jobs.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  DiscoveredClassWithMeta,
+  DiscoveryService,
+} from '@golevelup/nestjs-discovery';
+import { JOB_METADATA_KEY } from '../decorators/job.decorator';
+import { AbstractJob } from './job';
+
+@Injectable()
+export class JobsService implements OnModuleInit {
+  private jobs: DiscoveredClassWithMeta<AbstractJob>[] = [];
+
+  constructor(private readonly discoveryService: DiscoveryService) {}
+
+  async onModuleInit() {
+    this.jobs =
+      await this.discoveryService.providersWithMetaAtKey<AbstractJob>(
+        JOB_METADATA_KEY,
+      );
+  }
+}
