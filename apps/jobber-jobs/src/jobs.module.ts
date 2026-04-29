a26857d4ace2ee68743a9f75a39212b488175ebc
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-04-28 21:30:44 -0300

    feat(jobs): add DiscoveryModule import to JobsModule

--- apps/jobber-jobs/src/jobs.module.ts
+++ apps/jobber-jobs/src/jobs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './app/jobs/fibonacci.job';
+import { DiscoveryModule } from '@nestjs/core';
 
 @Module({
+  imports: [DiscoveryModule],
   providers: [FibonacciJob],
 })
 export class JobsModule {}
