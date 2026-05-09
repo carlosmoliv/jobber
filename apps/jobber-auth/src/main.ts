057af46641b5186ec59d9f520987c7d8377322fb
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-05-09 17:51:38 -0300

    feat(auth): integrate gRPC microservice and update auth service methods

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,14 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser());
   const port = app.get(ConfigService).getOrThrow('PORT');
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'auth',
+      protoPath: join(__dirname, 'proto', 'auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
