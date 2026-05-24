e936340b371704392d77ae20c7daa6ed43a5e312
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-05-24 12:39:32 -0300

    feat(auth): implement AuthController and integrate it into AuthModule

---
+++ apps/jobber-auth/src/app/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '@nestjs/common';
+import { Observable } from 'rxjs';
+//TODO: Fix the import path to avoid the module boundaries issue
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import {
+  AuthenticateRequest,
+  AuthServiceController,
+  AuthServiceControllerMethods,
+  User,
+} from 'types/proto/auth';
+
+@Controller()
+@AuthServiceControllerMethods()
+export class AuthController implements AuthServiceController {
+  authenticate(
+    request: AuthenticateRequest,
+  ): Promise<User> | Observable<User> | User {
+    return {} as User;
+  }
+}
--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,11 +1,28 @@
+import { join } from 'path';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './fibonacci.job';
 import { DiscoveryModule } from '@nestjs/core';
 import { JobsResolver } from './jobs.resolver';
 import { JobsService } from './jobs.service';
+// TODO: Fix the import path to avoid the module boundaries issue
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 
 @Module({
-  imports: [DiscoveryModule],
+  imports: [
+    DiscoveryModule,
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(__dirname, 'proto', 'auth.proto'),
+        },
+      },
+    ]),
+  ],
   providers: [FibonacciJob, JobsResolver, JobsService],
 })
 export class JobsModule {}
