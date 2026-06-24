82c147dadb2467d6ddf821fecfcdd23fb8726b04
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-06-24 16:28:35 -0300

    feat(libs): add abstract Pulsar consumer

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,8 +1,7 @@
 import { Producer } from 'pulsar-client';
 import { PulsarClient } from '@jobber/pulsar';
-import { OnModuleDestroy } from '@nestjs/common';
 
-export abstract class AbstractJob<T> implements OnModuleDestroy {
+export abstract class AbstractJob<T> {
   private producer: Producer;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
@@ -12,11 +11,5 @@ export abstract class AbstractJob<T> implements OnModuleDestroy {
       this.producer = await this.pulsarClient.createProducer(jobName);
     }
     await this.producer.send({ data: Buffer.from(JSON.stringify(data)) });
-  }
-
-  async onModuleDestroy() {
-    if (this.producer) {
-      await this.producer.close();
-    }
   }
 }
