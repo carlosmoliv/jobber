8167c1d22f6746b239f3d90b888b5d0350eb1bef
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-07-18 16:29:46 -0300

    refactor(apps): simplify bootstrap process by removing unnecessary code

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -1,20 +1,12 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { setupApp } from '@jobber/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
-  app.setGlobalPrefix(globalPrefix);
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
+  await setupApp(app);
   app.connectMicroservice<GrpcOptions>({
     transport: Transport.GRPC,
     options: {
@@ -23,10 +15,6 @@ async function bootstrap() {
     },
   });
   await app.startAllMicroservices();
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
 }
 
 bootstrap();
--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -1,22 +1,10 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+import { setupApp } from '@jobber/nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
-  app.setGlobalPrefix(globalPrefix);
-  app.use(cookieParser());
-  const port = app.get(ConfigService).getOrThrow('PORT');
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  await setupApp(app);
 }
 
 bootstrap();
