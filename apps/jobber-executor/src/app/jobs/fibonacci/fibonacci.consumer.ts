401d5da3988d517bde2d4eb1e5984a57c0b3581e
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-07-21 23:00:52 -0300

    feat: add Fibonacci job consumer and improve Pulsar integration

--- apps/jobber-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
+++ apps/jobber-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
@@ -1,17 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PulsarClient, PulsarConsumer } from '@jobber/pulsar';
-import { Message } from 'pulsar-client';
+import { FibonacciData } from './fibonacci-data.interface';
+import { iterate } from 'fibonacci';
 
 const FIBONACCI_TOPIC = 'Fibonacci';
 
 @Injectable()
-export class FibonacciConsumer extends PulsarConsumer implements OnModuleInit {
+export class FibonacciConsumer
+  extends PulsarConsumer<FibonacciData>
+  implements OnModuleInit
+{
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient, FIBONACCI_TOPIC);
   }
 
-  protected async onMessage(message: Message): Promise<void> {
-    console.log(`Received message: ${message.getData().toString()}`);
-    await this.acknowledge(message);
+  protected async onMessage(data: FibonacciData): Promise<void> {
+    const result = iterate(data.iterations);
+    this.logger.debug(result);
   }
 }
--- libs/pulsar/src/lib/pulsar.consumer.ts
+++ libs/pulsar/src/lib/pulsar.consumer.ts
@@ -1,8 +1,11 @@
 import { Consumer, Message } from 'pulsar-client';
 import { PulsarClient } from './pulsar.client';
+import { deserialize } from './serialize';
+import { Logger } from '@nestjs/common';
 
-export abstract class PulsarConsumer {
+export abstract class PulsarConsumer<T> {
   private consumer!: Consumer;
+  protected readonly logger: Logger = new Logger(PulsarConsumer.name);
 
   constructor(
     private readonly pulsarClient: PulsarClient,
@@ -12,13 +15,21 @@ export abstract class PulsarConsumer {
   async onModuleInit() {
     this.consumer = await this.pulsarClient.createConsumer(
       this.topic,
-      this.onMessage.bind(this),
+      this.listener.bind(this),
     );
   }
 
-  protected async acknowledge(message: Message) {
-    await this.consumer.acknowledge(message);
+  private async listener(message: Message) {
+    try {
+      const data = deserialize<T>(message.getData());
+      this.logger.debug(`Received message: ${JSON.stringify(data)}`);
+      await this.onMessage(data);
+    } catch (error) {
+      this.logger.error('Error occurred while processing message', error);
+    } finally {
+      await this.consumer.acknowledge(message);
+    }
   }
 
-  protected abstract onMessage(message: Message): Promise<void>;
+  protected abstract onMessage(message: T): Promise<void>;
 }
