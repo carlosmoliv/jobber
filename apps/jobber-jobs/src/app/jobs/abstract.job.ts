2d4d5e0f4f0a966abc25b91c930b9e0d591f82dd
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-08-01 13:41:27 -0300

    feat(jobs): improve AbstractJob with data validation and update FibonacciJob message class

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,15 +1,28 @@
 import { Producer } from 'pulsar-client';
 import { PulsarClient, serialize } from '@jobber/pulsar';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
-export abstract class AbstractJob<T> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
+  protected abstract messageClass: new () => T;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, jobName: string): Promise<void> {
+    await this.validateData(data);
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(jobName);
     }
     await this.producer.send({ data: serialize(data) });
+  }
+
+  private async validateData(data: T): Promise<void> {
+    const instance = plainToInstance(this.messageClass, data);
+    const errors = await validate(instance);
+
+    if (errors.length && errors.length > 0) {
+      throw new Error(`Job data is invalid: ${JSON.stringify(errors)}`);
+    }
   }
 }
--- apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
+++ apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -1,13 +1,15 @@
 import { PulsarClient } from '@jobber/pulsar';
 import { AbstractJob } from '../abstract.job';
 import { Job } from '../../decorators/job.decorator';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generates a Fibonacci sequence and store in the DB.',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
+
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }
