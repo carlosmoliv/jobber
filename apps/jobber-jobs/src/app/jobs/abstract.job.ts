9503bc3da753e33d8afd6598982715414eb38926
Carlos Oliveira <carlosmoliveira93@gmail.com>
2026-08-09 12:32:50 -0300

    refactor(jobs): handle jobs in batches in AbstractJob

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -2,6 +2,7 @@ import { Producer } from 'pulsar-client';
 import { PulsarClient, serialize } from '@jobber/pulsar';
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
 
 export abstract class AbstractJob<T extends object> {
   private producer: Producer;
@@ -10,19 +11,30 @@ export abstract class AbstractJob<T extends object> {
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, jobName: string): Promise<void> {
-    await this.validateData(data);
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(jobName);
     }
+    if (Array.isArray(data)) {
+      for (const message of data) {
+        await this.sendMessage(message);
+      }
+      return;
+    }
+    await this.sendMessage(data);
+  }
+
+  private async sendMessage(data: T): Promise<void> {
+    await this.validateData(data);
     await this.producer.send({ data: serialize(data) });
   }
 
   private async validateData(data: T): Promise<void> {
-    const instance = plainToInstance(this.messageClass, data);
-    const errors = await validate(instance);
+    const errors = await validate(plainToInstance(this.messageClass, data));
 
-    if (errors.length && errors.length > 0) {
-      throw new Error(`Job data is invalid: ${JSON.stringify(errors)}`);
+    if (errors.length) {
+      throw new BadRequestException(
+        `Job data is invalid: ${JSON.stringify(errors)}`,
+      );
     }
   }
 }
